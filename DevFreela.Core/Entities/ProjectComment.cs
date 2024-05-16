8d0ba605618c5b26ecd37a35836aa8fbff8fceb9
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-05-15 23:57:46 -0300

    Criado camada de Domain

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,18 @@
+﻿namespace DevFreela.Core.Entities;
+
+public class ProjectComment : BaseEntity
+{
+    public ProjectComment(int idProject, int idUser, string content)
+    {
+        IdProject = idProject;
+        Content = content;
+        IdUser = idUser;
+
+        CreatedAt = DateTime.Now;
+    }
+
+    public string Content { get; private set; }
+    public int IdProject { get; private set; }
+    public int IdUser { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+}
---
+++ DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,13 @@
+﻿namespace DevFreela.Core.Entities;
+
+public class UserSkill : BaseEntity
+{
+    public UserSkill(int idSkill, int idUser)
+    {
+        IdSkill = idSkill;
+        IdUser = idUser;
+    }
+
+    public int IdUser { get; private set; }
+    public int IdSkill { get; private set; }
+}
