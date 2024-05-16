fd0780a4a26bb7aa77ada46f57bdbcaa8187f17b
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-05-16 13:22:09 -0300

    modificado entity

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -23,8 +23,39 @@ public class Project : BaseEntity
     public int IdFreelancer { get; private set; }
     public decimal TotalCost { get; private set; }
     public DateTime CreatedAt { get; private set; }
-    public DateTime? StartedAt { get; }
-    public DateTime? FinishedAt { get; }
+    public DateTime? StartedAt { get; private set; }
+    public DateTime? FinishedAt { get; private set; }
     public ProjectStatusEnum Status { get; private set; }
     public List<ProjectComment> Comments { get; private set; }
+
+    public void Cancel()
+    {
+        if (Status is ProjectStatusEnum.InProgress or ProjectStatusEnum.Created)
+            Status = ProjectStatusEnum.Cancelled;
+    }
+
+    public void Start()
+    {
+        if (Status == ProjectStatusEnum.Created)
+        {
+            Status = ProjectStatusEnum.InProgress;
+            StartedAt = DateTime.Now;
+        }
+    }
+
+    public void Finish()
+    {
+        if (Status == ProjectStatusEnum.InProgress)
+        {
+            Status = ProjectStatusEnum.Finished;
+            FinishedAt = DateTime.Now;
+        }
+    }
+
+    public void Update(string title, string description, decimal totalCost)
+    {
+        Title = title;
+        Description = description;
+        TotalCost = totalCost;
+    }
 }
