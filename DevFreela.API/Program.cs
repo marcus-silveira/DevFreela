8685a29bddf5db9d9dc862e06ac236c3117942b0
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-05-16 22:43:00 -0300

    Criado ajustes para o banco de dados

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,8 +1,10 @@
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
-using DevFreela.Infra.Persistence;
+using DevFreela.Infra.DataBase.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SqlServerDocker");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,7 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<DevFreelaDbContext>();
+builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ISkillService, SkillService>();
