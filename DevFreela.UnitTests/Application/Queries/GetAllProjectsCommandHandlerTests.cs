dc6ceb211c4f276ec9741a6bc797fb6b51f5d1bc
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-05-23 13:48:20 -0300

    Criado testes de unidade com Moq

---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Queries;
+
+public class GetAllProjectsCommandHandlerTests
+{
+    [Fact]
+    public async Task ThreeProjectExists_Executed_ReturnThreeProjectsViewModels()
+    {
+        // Arrange
+        var projectList = new List<Project>
+        {
+            new("Project Test 1", "this is a test project", 1, 2, 1500),
+            new("Project Test 2", "this is a test project", 1, 2, 2000),
+            new("Project Test 3", "this is a test project", 1, 2, 4000),
+        };
+        var projectRepository = new Mock<IProjectRepository>();
+        projectRepository.Setup(p => p.GetAllAsync(CancellationToken.None)).ReturnsAsync(projectList);
+
+        var query = new GetAllProjectsQuery("");
+        var queryHandler = new GetAllProjectsQueryHandler(projectRepository.Object);
+
+        // Act
+        var sut = await queryHandler.Handle(query, CancellationToken.None);
+        
+        // Assert
+        Assert.NotEmpty(sut);
+        Assert.NotNull(sut);
+        Assert.Equal(sut.Count, projectList.Count);
+        projectRepository.Verify(p => p.GetAllAsync(CancellationToken.None).Result, Times.Once);
+    }
+}
---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTests.Core.Entities;
+
+public class ProjectTests
+{
+    [Fact]
+    public void Start_StatusIsCreatedEqual_StartProject()
+    {
+        // Arrange
+        var project = new Project("Project Test", "this is a test project", 1, 2, 1500);
+
+        // Act
+        project.Start();
+        
+        // Assert
+        Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+        Assert.NotNull(project.StartedAt);
+    }
+    
+    [Fact]
+    public void Start_StatusIsNotCreatedEqual_NotStartProject()
+    {
+        // Arrange
+        var project = new Project("Project Test", "this is a test project", 1, 2, 1500);
+        project.Start();
+        project.Finish();
+        
+        // Act
+        project.Start();
+        
+        // Assert
+        Assert.NotEqual(ProjectStatusEnum.InProgress, project.Status);
+        Assert.Equal(ProjectStatusEnum.Finished, project.Status);
+    }
+}
